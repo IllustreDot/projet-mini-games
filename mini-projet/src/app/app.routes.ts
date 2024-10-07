0f22f7f91990cf8bf4b8da1f54d0f3b963065543
cleo.eiras <cleo.eiras@etu.uca.fr>
2024-10-07 09:56:50 +0200

    loss

--- mini-projet/src/app/app.routes.ts
+++ mini-projet/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import { ConnexionPageComponent } from './connexion-page/connexion-page.component';
+import { ConnexionPageComponent } from './components/connexion-page/connexion-page.component';
 import { HomeComponent } from './components/home/home.component';
+import { LostComponent } from './components/lost/lost.component';
 
 export const routes: Routes = [
+    {path: 'lost', component: LostComponent},
     {path: 'connexion', component: ConnexionPageComponent},
     {path:'home', component:HomeComponent},
+    { path: '**', redirectTo: 'lost'}
 ];
